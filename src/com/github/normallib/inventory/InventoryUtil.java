326f0604942f0e19457b8f68f83d4977817543c6
Wenr-Wenrou <127990606+Wenr-Wenrou@users.noreply.github.com>
2023-03-20 16:38:34 +0800

    Create InventoryUtil.java

---
+++ src/com/github/normallib/inventory/InventoryUtil.java
@@ -0,0 +1,28 @@
+package com.github.normallib.inventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class InventoryUtil {
+
+    public static void forEachItem(Inventory inventory, Consumer<ItemStack> action) {
+        for (ItemStack item : inventory.getContents()) {
+            if (item != null) {
+                action.accept(item);
+            }
+        }
+    }
+
+    public static void forEachItem(Inventory inventory, Predicate<ItemStack> condition, Consumer<ItemStack> action) {
+        for (ItemStack item : inventory.getContents()) {
+            if (item != null && condition.test(item)) {
+                action.accept(item);
+            }
+        }
+    }
+}
